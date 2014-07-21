1f80d5f89a83a5a148c733eaf2e002da82fcf7e5
Artur6695 <artur6695@yandex.ru>
2014-07-22 00:48:41 +0600

    Update Predlojenie.cs

--- Predlojenie.cs
+++ Predlojenie.cs
@@ -8,17 +8,19 @@ namespace _1
 {
     class Predlojenie
     {
-        public int _maxChars = 0;                          
-        public string _sentance = "";                       
+        public int _maxWords = 0;  
+        public string _sentance = "";                   
         public string[] _words = {};                        
-        public int _chars = 0;                             
+        public int _chars = 0;                              
                                                             
                                                             
         private char[] _ch = { '.', ',', '!', '?', '/', ':', ';', '"', '-' };
-        public Predlojenie(int maxChars, string sentance)
+                                                        
+        public Predlojenie(int maxWords, string sentance)
         {
-            _maxChars = maxChars;
+            _maxWords = maxWords;
             _sentance = sentance;
+
             for (int i = 0; i < _sentance.Length; i++)
             {
                 for (int j = 0; j < _ch.Length; j++)
@@ -36,19 +38,16 @@ namespace _1
             string sentance = "";
             for (int i = _words.Length - 1; i >= 0; i--)
             {
-
                 string[] word = _words[i].Split(_ch);
                 sentance += word[0].ToLower() + " ";
             }
             return sentance;
         }
 
-        public string SearchSentanse(int index)
+        public string SearchWord(int index)
         {
-      
             if (index > 0 && index < _words.Length)
             {
-           
                 string[] word = _words[index].Split(_ch);
                 return word[0];
             }
